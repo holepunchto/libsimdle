31c991117dacfaff9c19b5464c735c938a405ce5
Kasper Isager Dalsgarð <kasperisager@hey.com>
2022-08-26 14:32:30 +0200

    Test `not`

---
+++ test/not/v128-u8.c
@@ -0,0 +1,32 @@
+#include <assert.h>
+
+#include "../../include/simd.h"
+
+int
+main () {
+  simd_v128_t v = {
+    .u8 = {
+      0b00000000,
+      0b00000001,
+      0b00000011,
+      0b00000111,
+      0b00001111,
+      0b00011111,
+      0b00111111,
+      0b01111111,
+      0b11111111,
+    },
+  };
+
+  simd_v128_t r = simd_not_v128_u8(v);
+
+  assert(r.u8[0] == 0b11111111);
+  assert(r.u8[1] == 0b11111110);
+  assert(r.u8[2] == 0b11111100);
+  assert(r.u8[3] == 0b11111000);
+  assert(r.u8[4] == 0b11110000);
+  assert(r.u8[5] == 0b11100000);
+  assert(r.u8[6] == 0b11000000);
+  assert(r.u8[7] == 0b10000000);
+  assert(r.u8[8] == 0b00000000);
+}
