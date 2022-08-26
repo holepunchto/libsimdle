c5f7094ad9fbd4147d8b5d03e166d4ce966e9d9e
Kasper Isager Dalsgarð <kasperisager@hey.com>
2022-08-26 11:49:51 +0200

    Use subscript notation

--- src/not.c
+++ src/not.c
@@ -10,9 +10,7 @@ simd_not_v128_u8 (simd_v128_t vec) {
 #if defined(SIMD_ARCH_ARM)
   vec.u8 = vmvnq_u8(vec.u8);
 #else
-  for (int i = 0; i < 16; i++) {
-    vec.u8[i] = ~vec.u8[i];
-  }
+  vec.u8 = ~vec.u8;
 #endif
 
   return vec;
@@ -23,9 +21,7 @@ simd_not_v128_u16 (simd_v128_t vec) {
 #if defined(SIMD_ARCH_ARM)
   vec.u16 = vmvnq_u16(vec.u16);
 #else
-  for (int i = 0; i < 8; i++) {
-    vec.u16[i] = ~vec.u16[i];
-  }
+  vec.u16 = ~vec.u16;
 #endif
 
   return vec;
@@ -36,9 +32,7 @@ simd_not_v128_u32 (simd_v128_t vec) {
 #if defined(SIMD_ARCH_ARM)
   vec.u32 = vmvnq_u32(vec.u32);
 #else
-  for (int i = 0; i < 4; i++) {
-    vec.u32[i] = ~vec.u32[i];
-  }
+  vec.u32 = ~vec.u32;
 #endif
 
   return vec;
