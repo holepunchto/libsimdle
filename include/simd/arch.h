9e26e941413102d13727b6e4ba9488e2682a4265
Kasper Isager Dalsgarð <kasperisager@hey.com>
2022-08-31 14:12:44 +0200

    Expand Intel feature detection

--- include/simd/arch.h
+++ include/simd/arch.h
@@ -23,6 +23,18 @@
 #if defined(__SSE2__)
 #define SIMD_ARCH_INTEL_SSE2
 #endif
+#if defined(__AVX__)
+#define SIMD_ARCH_INTEL_AVX
+#endif
+#if defined(__AVX512CD__)
+#define SIMD_ARCH_INTEL_AVX512CD
+#endif
+#if defined(__AVX512VL__)
+#define SIMD_ARCH_INTEL_AVX512VL
+#endif
+#if defined(__BMI__)
+#define SIMD_ARCH_INTEL_BMI
+#endif
 #endif
 
 // Includes
@@ -31,7 +43,9 @@
 #include <arm_neon.h>
 #endif
 
-#if defined(SIMD_ARCH_INTEL_SSE2)
+#if defined(SIMD_ARCH_INTEL_AVX) || defined(SIMD_ARCH_INTEL_BMI)
+#include <immintrin.h>
+#elif defined(SIMD_ARCH_INTEL_SSE2)
 #include <emmintrin.h>
 #endif
 
