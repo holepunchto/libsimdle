22c6367dd4dfedfa80895bf183073685805e9096
Kasper Isager Dalsgarð <kasperisager@hey.com>
2022-09-05 14:24:32 +0200

    `cto` should use `ctz`

--- include/simdle/cto.h
+++ include/simdle/cto.h
@@ -13,7 +13,7 @@ simdle_cto_v128_u8 (simdle_v128_t vec) {
 #if defined(SIMDLE_ARCH_ARM_NEON)
   vec.u8 = vclzq_u8(vmvnq_u8(vrbitq_u8(vec.u8)));
 #else
-  vec = simdle_clo_v128_u8(simdle_not_v128_u8(vec));
+  vec = simdle_ctz_v128_u8(simdle_not_v128_u8(vec));
 #endif
 
   return vec;
@@ -24,7 +24,7 @@ simdle_cto_v128_u16 (simdle_v128_t vec) {
 #if defined(SIMDLE_ARCH_ARM_NEON)
   vec.u16 = vclzq_u16(vrev16q_u8(vmvnq_u16(vrbitq_u8(vec.u8))));
 #else
-  vec = simdle_clo_v128_u16(simdle_not_v128_u16(vec));
+  vec = simdle_ctz_v128_u16(simdle_not_v128_u16(vec));
 #endif
 
   return vec;
@@ -35,7 +35,7 @@ simdle_cto_v128_u32 (simdle_v128_t vec) {
 #if defined(SIMDLE_ARCH_ARM_NEON)
   vec.u32 = vclzq_u32(vrev32q_u8(vmvnq_u32(vrbitq_u8(vec.u8))));
 #else
-  vec = simdle_clo_v128_u32(simdle_not_v128_u32(vec));
+  vec = simdle_ctz_v128_u32(simdle_not_v128_u32(vec));
 #endif
 
   return vec;
